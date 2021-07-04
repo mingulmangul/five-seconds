4fba290162afd3e324976dba02f141dc416fcdf8
Ming <71026706+mingulmangul@users.noreply.github.com>
2021-07-05 04:41:59 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,10 +1,26 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-clonecoding",
+  acl: "public-read",
+});
 
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
 
 export const avatarUpload = multer({
@@ -12,6 +28,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
 
 export const localsMiddleware = (req, res, next) => {
